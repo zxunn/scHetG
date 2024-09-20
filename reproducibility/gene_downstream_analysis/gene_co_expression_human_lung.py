273a9d856e065bd9e23cce40d8f5cd84a61d9dd1
Xun Zhang <108138936+zxunn@users.noreply.github.com>
2024-09-20 16:02:25 +0800

    Add files via upload

---
+++ reproducibility/gene_downstream_analysis/gene_co_expression_human_lung.py
@@ -0,0 +1,146 @@
+import scanpy as sc
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+from pca import pca
+import warnings
+warnings.filterwarnings('ignore')
+from sklearn.preprocessing import LabelEncoder
+from utlis import softmax, reduce_dimensions, cal_ratio
+import pandas as pd
+
+
+def add_geneset_human_lung(dataset, gene_embeddings, gene_names, method):
+    csv_data = pd.read_csv('marker_genes/'+dataset+".csv")
+    markergene = csv_data['Cell marker'].values
+    common_genes = np.intersect1d(markergene, gene_names)
+    common_genes_indices = np.concatenate([np.where(markergene == gene)[0] for gene in common_genes])
+    markergene = markergene[common_genes_indices]
+    cellname = csv_data['Cell name'].values[common_genes_indices]
+
+    ## Set selected cell type/group
+    all_ct = np.unique(cellname)
+    selected_ct = np.array(['lung macrophage', 'lung endothelial cell', 'lung secretory cell','B cell'], dtype=object)
+    gs_dict_new = {k: markergene[cellname == k] for k in all_ct}
+    gs_dict_comb_subset = {k: markergene[cellname == k] for k in selected_ct}
+
+    ## Get mutually exclusive sets
+    gs_dict_excl = {}
+    for gs_name, gs_genes in gs_dict_comb_subset.items():
+        for gs_name2, gs_genes2 in gs_dict_new.items():
+            if gs_name != gs_name2:
+                gs_genes = np.setdiff1d(gs_genes, gs_genes2)
+        gs_dict_excl[gs_name] = gs_genes
+
+    X_plot, dim_labels = reduce_dimensions(gene_embeddings, method='PCA', reduced_dimension=2)
+    df_plot = pd.concat([pd.DataFrame(X_plot), pd.DataFrame(gene_names)], axis=1)
+    df_plot.columns = dim_labels + ["Label"]
+    df_plot.index = gene_names
+    df_plot['Geneset'] = 'None'
+    gs_dict_in = gs_dict_excl
+    for gs_, genes in gs_dict_in.items():
+        df_plot['Geneset'].loc[np.isin(df_plot.index, genes)] = gs_
+
+    ratio, L, celltype = cal_ratio(gene_embeddings, df_plot)
+    print(method, " inter/intra ratio: ", ratio)
+
+    df_plot = df_plot[df_plot.Geneset != 'None']
+    return df_plot, dim_labels, L, celltype
+
+
+######## huamn lung #####################
+
+data = sc.read_h5ad("../../gene&cell_emb_human_lung_scHetG")  # gene embeddings obtained by scHetG
+SIMBA_data = sc.read_h5ad("../../gene&cell_emb_human_lung_SIMBA")  # gene embeddings obtained by SIMBA
+
+colors = ['#4878D0', 'gold', '#6ACC64', '#D65F5F', 'lightpink', 'teal', 'navy', 'darkorchid','#FF0000','#00FF00', '#0000FF', '#FFFF00','#FF6600']
+legend_labels = ['B cell','lung macrophage', 'lung endothelial cell', 'lung secretory cell']
+legend_handles = [plt.Line2D([0], [0], marker='o', color='w', markerfacecolor=color, markersize=8) for color in
+                  colors]
+fig, ax = plt.subplots(1, 3, constrained_layout=True, figsize=(30,8))
+
+#Ensure that different methods use the same genes
+data_genes = data.var_names
+simba_genes = SIMBA_data.obs_names
+common_genes = np.intersect1d(data_genes, simba_genes)
+data_genes_indices = np.concatenate([np.where(data_genes == gene)[0] for gene in common_genes])
+simba_genes_indices = np.concatenate([np.where(simba_genes == gene)[0] for gene in common_genes])
+
+model = pca(n_components=64)
+X_reduction = model.fit_transform(data.X.T)
+gene_embeddings, gene_names = X_reduction['PC'].values, data.var_names
+gene_embeddings = gene_embeddings[data_genes_indices]
+gene_names = gene_names[data_genes_indices]
+df_plot, dim_labels, L1,celltype = add_geneset_human_lung("human lung", gene_embeddings, gene_names, "normalized data")
+df_plot.insert(loc=4, column='gene', value=LabelEncoder().fit_transform(df_plot.Geneset.values).astype(np.int_))
+ax0 = sns.scatterplot(x=dim_labels[0], y=dim_labels[1], hue='Geneset', data=df_plot, legend=False,palette=colors,
+                      edgecolor='black', linewidth=0.2, s=50, ax=ax[0])
+ax0.set_title("normalized data", fontsize=36)
+#remove_spines(ax0)
+ax0.xaxis.set_visible(False)
+ax0.yaxis.set_visible(False)
+
+need_gene = df_plot.index
+
+gene_embeddings = SIMBA_data.X
+gene_names = SIMBA_data.obs_names
+gene_embeddings = gene_embeddings[simba_genes_indices]
+gene_names = gene_names[simba_genes_indices]
+df_plot, dim_labels, L2,celltype = add_geneset_human_lung("human lung", gene_embeddings, gene_names, "SIMBA")
+df_plot.insert(loc=4, column='gene', value=LabelEncoder().fit_transform(df_plot.Geneset.values).astype(np.int_))
+ax1 = sns.scatterplot(x=dim_labels[0], y=dim_labels[1], hue='Geneset', data=df_plot, legend=False, palette=colors,
+                      edgecolor='black', linewidth=0.2, s=50, ax=ax[1])
+ax1.set_title("SIMBA", fontsize=36)
+ax1.xaxis.set_visible(False)
+ax1.yaxis.set_visible(False)
+
+gene_embeddings = data.varm['feat']
+gene_embeddings = softmax(data, adata_ref=data.obsm['feat'], adata_query=gene_embeddings)
+gene_names = data.var_names
+gene_embeddings = gene_embeddings[data_genes_indices]
+gene_names = gene_names[data_genes_indices]
+df_plot, dim_labels, L3,celltype = add_geneset_human_lung("human lung", gene_embeddings, gene_names, "scHetG")
+df_plot.insert(loc=4, column='gene', value=LabelEncoder().fit_transform(df_plot.Geneset.values).astype(np.int_))
+ax2 = sns.scatterplot(x=dim_labels[0], y=dim_labels[1], hue='Geneset', data=df_plot, legend=False, palette=colors,
+                      edgecolor='black', linewidth=0.2, s=50, ax=ax[2])
+ax2.set_title("scHetG", fontsize=36)
+ax2.xaxis.set_visible(False)
+ax2.yaxis.set_visible(False)
+
+plt.rcParams['font.family'] = 'Arial'
+plt.rcParams['legend.fontsize'] = 28
+fig.legend(legend_handles, legend_labels, loc='right',frameon=False,markerscale=2.)
+plt.tight_layout(rect=[0, 0, 0.8, 1])
+plt.savefig('figure_co/human_lung.png', format='png', dpi=900)
+plt.show()
+
+########heat graph
+
+df1 = pd.DataFrame(L1, index=np.unique(celltype), columns=np.unique(celltype))
+df2 = pd.DataFrame(L2, index=np.unique(celltype), columns=np.unique(celltype))
+df3 = pd.DataFrame(L3, index=np.unique(celltype), columns=np.unique(celltype))
+
+
+fig, axes = plt.subplots(1, 3, figsize=(24, 8))
+
+
+sns.heatmap(df1, annot=True, fmt=".2f", cmap="YlGnBu", ax=axes[0])
+axes[0].set_title('normalized data')
+axes[0].tick_params(axis='x', rotation=0)
+
+sns.heatmap(df2, annot=True, fmt=".2f", cmap="YlGnBu", ax=axes[1])
+axes[1].set_title('SIMBA')
+axes[1].tick_params(axis='x', rotation=0)
+
+sns.heatmap(df3, annot=True, fmt=".2f", cmap="YlGnBu", ax=axes[2])
+axes[2].set_title('scHetG')
+axes[2].tick_params(axis='x', rotation=0)
+
+
+plt.tight_layout()
+plt.savefig('heat_graph/human_lung.png', format='png', dpi=900)
+
+plt.show()
+
+
---
+++ reproducibility/gene_downstream_analysis/utlis.py
@@ -0,0 +1,188 @@
+from scipy.spatial import distance
+import scanpy as sc
+from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
+import umap.umap_ as umap
+import numpy as np
+import warnings
+warnings.filterwarnings('ignore')
+
+
+def preprocess(adata, filter_min_counts=True, size_factors=True, normalize_input=False, logtrans_input=True):
+    if size_factors or normalize_input or logtrans_input:
+        adata.raw = adata.copy()
+    else:
+        adata.raw = adata
+
+    if filter_min_counts:
+        sc.pp.filter_genes(adata, min_cells=3)
+        sc.pp.filter_cells(adata, min_genes=200)
+
+    if size_factors:
+        sc.pp.normalize_per_cell(adata)
+        adata.obs['cs_factor'] = adata.obs.n_counts / np.median(adata.obs.n_counts)
+    else:
+        adata.obs['cs_factor'] = 1.0
+
+    if logtrans_input:
+        sc.pp.log1p(adata)
+
+    if normalize_input:
+        sc.pp.scale(adata)
+
+    return adata
+
+def softmax(data,adata_ref,
+            adata_query,
+            T=0.5,
+            n_top=None,
+            percentile=0):
+    """Softmax-based transformation
+
+    This will transform query data to reference-comparable data
+
+    Parameters
+    ----------
+    adata_ref: `AnnData`
+        Reference anndata.
+    adata_query: `list`
+        Query anndata objects
+    T: `float`
+        Temperature parameter.
+        It controls the output probability distribution.
+        When T goes to inf, it becomes a discrete uniform distribution,
+        each query becomes the average of reference;
+        When T goes to zero, softargmax converges to arg max,
+        each query is approximately the best of reference.
+    n_top: `float` (default: None)
+        The number of top reference entities to include when transforming
+        entities in query data.
+        It is used to filter out low-probability reference entities.
+        All reference entities used by default.
+    percentile: `int` (default: 0)
+        Percentile (0-100) of reference entities to exclude when transforming
+        entities in query data.
+        Only valid when `n_top` is not specified.
+        It is used to filter out low-probability reference entities.
+        All reference entities used by default.
+
+    Returns
+    -------
+    updates `adata_query` with the following field.
+    softmax: `array_like` (`.layers['softmax']`)
+        Store #observations × #dimensions softmax transformed data matrix.
+    """
+
+    #scores_ref_query = np.matmul(adata_ref, adata_query.T)
+    #scores_ref_query = data.X.toarray()
+    scores_ref_query = data.X
+    # data2 = data.copy()
+    # data2.X = data.obsm['edge']
+    # data2 = preprocess(data2)
+    # scores_ref_query=data2.X
+
+    # avoid overflow encountered
+    scores_ref_query = scores_ref_query - scores_ref_query.max()
+    scores_softmax = np.exp(scores_ref_query/T) / \
+        (np.exp(scores_ref_query/T).sum(axis=0))[None, :]
+    if n_top is None:
+        thresh = np.percentile(scores_softmax, q=percentile, axis=0)
+    else:
+        thresh = (np.sort(scores_softmax, axis=0)[::-1, :])[n_top-1, ]
+    mask = scores_softmax < thresh[None, :]
+    scores_softmax[mask] = 0
+    # rescale to make scores add up to 1
+    scores_softmax = scores_softmax/scores_softmax.sum(axis=0, keepdims=1)
+    X_query = np.dot(scores_softmax.T, adata_ref)
+    #adata_query.layers['softmax'] = X_query
+    return X_query
+
+def reduce_dimensions(X, reduced_dimension = 2, method = 'PCA', tsne_min = 50, match_dims = True,**kwargs):
+    """
+    Reduce dimensioanlity of X using dimensionality reduction method.
+    """
+    if X.shape[-1] > reduced_dimension:
+        if method == "PCA":
+            pca = PCA(n_components = reduced_dimension,**kwargs)
+            pca.fit(X)
+            X_pca = pca.transform(X)
+            X_reduced = X_pca[:,:reduced_dimension]
+            dim_name = 'PC'
+        elif method == "tSNE":
+            if X.shape[-1] > tsne_min:
+                X,_ = reduce_dimensions(X, reduced_dimension = tsne_min, method = 'PCA')
+            tsne = TSNE(n_components = reduced_dimension, init = 'pca', **kwargs)
+            X_tsne = tsne.fit_transform(X)
+            X_reduced = X_tsne[:,:2]
+            dim_name = 't-SNE'
+        elif method == 'UMAP':
+            if X.shape[-1] > tsne_min:
+                X,_ = reduce_dimensions(X, reduced_dimension = tsne_min, method = 'PCA')
+            X_reduced = umap.UMAP(**kwargs).fit_transform(X)
+            X_reduced = X_reduced[:,:2]
+            dim_name = 'UMAP'
+        else:
+            raise Exception('{} is not a valid DR method (PCA,tSNE,UMAP)'.format(method))
+    else:
+        if X.shape[-1] < reduced_dimension and match_dims:
+            dim_diff = reduced_dimension - X.shape[-1]
+            X_reduced = np.concatenate([X, np.zeros([len(X),dim_diff])], axis = 1)
+        else:
+            X_reduced = X
+        dim_name = "Dim"
+    dim_labels = ["{} {}".format(dim_name, ii+1) for ii in range(reduced_dimension)]
+    return X_reduced, dim_labels
+
+
+def cal_ratio(gene_embeddings, df_plot):
+    G=gene_embeddings[df_plot.Geneset != 'None']
+    celltype =df_plot.Geneset[df_plot.Geneset != 'None'].values
+    sorted_index = np.argsort(celltype)
+    celltype=celltype[sorted_index]
+    G=G[sorted_index]
+
+    S = np.exp(-np.square(distance.cdist(G, G, 'euclidean')))
+    differences = celltype[1:] != celltype[:-1]
+    indices = np.where(differences)[0] + 1
+    indices = np.append(0, indices)
+    indices = np.append(indices, G.shape[0])
+
+    num_ctype = len(np.unique(celltype))
+    L=np.zeros((num_ctype,num_ctype))
+
+    for p in range(1,num_ctype+1):
+        for q in range(1,num_ctype+1):
+            if p == q:
+                L[p-1,q-1] = 1 / (np.square(indices[p] - indices[p-1]) - (indices[p] - indices[p-1]))
+                temp_s=S[indices[p-1]:indices[p],indices[p-1]:indices[p]]
+                sum_s = temp_s.sum()-np.trace(temp_s)
+                #sum_s = sum_s/2
+                if sum_s == 0:
+                    sum_s = np.amax(temp_s[~np.eye(temp_s.shape[0], dtype=bool)])
+                    #sum_s = 0.00001
+                L[p-1, q-1] = L[p-1,q-1]*sum_s
+            else:
+                L[p-1, q-1]=1 / ((indices[p] - indices[p-1]) * (indices[q] - indices[q-1]))
+                temp_s = S[indices[p-1]:indices[p],indices[q-1]:indices[q]]
+                sum_s = temp_s.sum()
+                L[p-1, q-1] = L[p-1, q-1] * sum_s
+
+    diagonal_L = np.diag(L)
+    L_new = L / diagonal_L[:, np.newaxis]
+    ratio = 1/(num_ctype*(num_ctype-1)) * (L_new.sum()-np.trace(L_new))
+
+    # import matplotlib.pyplot as plt
+    # import seaborn as sns
+    # import pandas as pd
+    # df = pd.DataFrame(
+    #     L,
+    #     index=np.unique(celltype),
+    #     columns=np.unique(celltype)
+    # )
+
+    # plt.figure(figsize=(10, 8))
+    # sns.heatmap(df, annot=True, fmt=".2f", cmap="YlGnBu")
+    # # plt.savefig('heat_graph/human pancreas.png', format='png', dpi=900)
+    # plt.show()
+
+    return ratio, L, celltype
